f8b99f6c719049550072b9b6341bb8fd093629a5
aJinhyunCho <Jinhyun.Cho@access-company.com>
2013-11-25 20:29:07 +0900

    test newsite

--- gittest/urls.py
+++ gittest/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'gittest.views.home', name='home'),
     url(r'^blog/', include('blog.urls')),
+    url(r'^newsite/', 'gittest.views.home', name='newsite'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
